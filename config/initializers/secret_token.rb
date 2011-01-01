5db20a0eca356016e5c031b7ad95ab0a5d3f8e81
Kosuke Matsuda <kosukematsuda@gmail.com>
2011-01-02 01:48:37 +0900

    create Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metaphor::Application.config.secret_token = 'b4629b750741b32458b34516c872686f977058890d72df599d63097957b3c1e9ea067b4283986128d7155d03cba4f64ffff7bef2f3af8826ed2aca56a2180fa9'
